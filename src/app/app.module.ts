d5a53cca2fa6815de2169545636117a0db2977cf
abir <abiramdouni20@gmail.com>
2021-01-10 12:57:28 +0100

    premier pas au tp

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PremierComponent } from './premier/premier.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PremierComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
